8a78e6ebce7e8cf392cf29b44d81e3b1dc4d5b4e
Rudy Affandi <rudy@adnetinc.com>
2025-02-24 10:17:52 -0400

    Fix aws cli export statements

--- synctos3.sh
+++ synctos3.sh
@@ -7,7 +7,7 @@ AWS_REGION=$3
 LOG_FILE="${2}/public/site/assets/logs/synctos3.txt"
 # Authentication
 export AWS_ACCESS_KEY_ID=$4
-export AWS_SECRET_KEY=$5
+export AWS_SECRET_ACCESS_KEY=$5
 # Sync command
 aws s3 sync "$LOCAL_DIRECTORY" "s3://$BUCKET_NAME/site/" --region "$AWS_REGION" --exclude "*" --include "*.css" --include "*.js" --include "*.jpg" --include "*.jpeg" --include "*.png" --include "*.gif" --include "*.svg"  --include "*.webp"  --include "*.pdf" --include "*.docx" --include "*.pptx"  --include "*.xlsx" --include "*.woff" --include "*.woff2" --include "*.ttf" --include "*.otf" --include "*.eot" --include "*.ico" --include "*.csv" --delete 
 
